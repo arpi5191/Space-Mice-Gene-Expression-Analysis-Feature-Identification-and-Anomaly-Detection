53cb8ec157cd86d2b97e77b8c465df285c81c90c
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2023-03-03 12:41:33 -0800

    Update compare-metaclusters.py

--- compare-metaclusters.py
+++ compare-metaclusters.py
@@ -12,23 +12,10 @@ class metaClusters:
 
     def getClusters(self, clusterNum1, clusterNum2):
         data = self.contents[clusterNum1:clusterNum2]
-        print(data)
         rez1 = cluster_analysis(data, n = 2, method = "kmeans")
         rez2 = cluster_analysis(data, n = 6, method = "kmeans")
         list_of_clusters = list(rez1, rez2)
         m = cluster_meta(list_of_clusters)
-
-# Visualize matrix without reordering
-# heatmap(m, Rowv = NA, Colv = NA, scale = "none") # Without reordering
-        # numSamples = len(self.contents)
-        # range = np.random.RandomState(0)
-        # covariance = np.array([[0.4, -0.5], [2.3, 0.9]])
-        # cluster_1 = 0.4 * range.randn(numSamples, 2) @ covariance + np.array([2, 2])
-        # cluster_2 = 0.3 * range.randn(numSamples, 2) + np.array([-2, -2])
-        # print(cluster_meta(cluster_1, cluster_2))
-        # data = self.contents[clusterNum1:clusterNum2]
-        # cluster_meta(list_of_clusters, rownames = NULL, ...)
-        # cluster1 = cluster_analysis(data, n = 3, method = "metacluster")
 
 def main(inFile = None):
     theMetaClusters = metaClusters(inFile)
