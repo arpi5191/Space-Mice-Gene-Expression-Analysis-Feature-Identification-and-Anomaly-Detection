bcd48afc7d4a428f1a17f9c6f9d916b372f5c70f
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2023-08-26 20:07:23 -0400

    Update Ibis_PCA.py

--- Ibis_PCA.py
+++ Ibis_PCA.py
@@ -1,6 +1,9 @@
+# Imports for the First Half of Code
+import os
 import sys
 import csv
 import ibis
+import sklearn
 import argparse
 import numpy as np
 import pandas as pd
@@ -23,7 +26,12 @@ from sklearn.compose import TransformedTargetRegressor
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.feature_selection import SelectKBest, f_classif
 
+# Adds the sklearn path to the code
+ibis.options.interactive = True
+sys.path.append('/Users/arpitha/Documents/Stuart_Lab/Ibis_Substrate/substrait-ml/sklearn_ibis')
+os.environ['PYTHONPATH'] = ':'.join(sys.path)
 
+# Command line options for the File
 parser = argparse.ArgumentParser()
 parser.add_argument('--inputFile1',default='data.csv',type=str,action='store',
                     help='input file goes here')
@@ -33,16 +41,19 @@ args = parser.parse_args()
 data=args.inputFile1
 metaData=args.inputFile2
 
+# Read the metaData File
 with open(metaData, "r") as csvfile:
     datareader = csv.reader(csvfile)
     for row in datareader:
         headers = row
         break
 
+# Obtain the Sample, Libprep Features & their Respective Indices
 features = ["sample", "libprep"]
 sample_index = headers.index(features[0])
 libprep_index = headers.index(features[1])
 
+# Obtain the libprep for Each Sample
 parameters = []
 libprepDict = dict()
 with open(metaData, 'r') as csvfile:
@@ -55,12 +66,14 @@ with open(metaData, 'r') as csvfile:
         parameters.append(values)
 parameters = np.array(parameters)
 
+# Remove First Sample
 with open(data) as csv_file:
     for s in csv.reader(csv_file, delimiter=','):
         samples = s
         break
 samples.remove(samples[0])
 
+# Obtain the X and y to perform the fitting on
 dataInfo = pd.read_csv(data)
 X = []
 y = []
@@ -82,10 +95,8 @@ pipe = Pipeline(steps=[
     ("scaler", StandardScaler()),
     ("kbest", SelectKBest(f_classif, k=1000)),
     ("pca", PCA(n_components=2)),
-    # ("gb", GradientBoostingClassifier()),
     ("logistic", LogisticRegression(max_iter=10000, tol=0.1)),
-    # ("rf", RandomForestClassifier(n_estimators=100)),
-], verbose=2)
+])
 
 #`'pca__n_components'`: A list of integers specifying the number of principal components to keep after PCA
    # transformation.
@@ -103,9 +114,86 @@ param_grid = {
 
 # You can use GridSearchCV to search over a range of hyperparameters for each step in the pipeline.
 # This can help you find the best combination of hyperparameters for your specific problem.
-grid_search = GridSearchCV(pipe, param_grid=param_grid, cv=5)
+grid_search = GridSearchCV(pipe, param_grid=param_grid, cv=2)
 
 pipe.fit(X, y)
+
+from sklearn.pipeline import Pipeline
+from ibis.expr.types import Table
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+# from sklearn_ibis.linear_model import LogisticRegressionIbis
+# from sklearn_ibis.decomposition import PCAIbis
+# from sklearn_ibis.preprocessing import StandardScalerIbis
+# from sklearn_ibis.ibis.ibis_transformer import IbisTransformer
+#
+#
+# class PipelineIbis:
+#     def __init__(self, wrapped: Pipeline):
+#         self.steps = wrapped.steps
+#
+#     def to_ibis(self):
+#         def fn(table: Table):
+#
+#             for step in self.steps:
+#                 step = step[1]
+#
+#                 if isinstance(step, StandardScaler):
+#                     wrapper = StandardScalerIbis(step)
+#                 elif isinstance(step, LogisticRegression):
+#                     wrapper = LogisticRegressionIbis(step)
+#                 elif isinstance(step, PCA):
+#                     wrapper = PCAIbis(step)
+#                 elif isinstance(step, IbisTransformer):
+#                     wrapper = step
+#                 else:
+#                     raise Exception(f"No Ibis implemntation found for {type(step)}")
+#
+#                 table = wrapper.to_ibis()(table)
+#
+#             return table
+#
+#         return fn
+
+class PipelineIbis:
+    def __init__(self, wrapped: Pipeline):
+        self.steps = wrapped.steps
+
+    def to_ibis(self):
+        def fn(table):
+
+            for step in self.steps:
+                step = step[1]
+
+                if isinstance(step, StandardScaler):
+                    wrapper = step.fit_transform(table)
+                    # wrapper = StandardScaler(step)
+                    print("HELLO")
+                elif isinstance(step, SelectKBest):
+                    wrapper = SelectKBest(step)
+                elif isinstance(step, PCA):
+                    wrapper = PCA(step)
+                elif isinstance(step, LogisticRegression):
+                    wrapper = LogisticRegression(step)
+                    table = wrapper.to_ibis()(table)
+                else:
+                    raise Exception(f"No Ibis implementation found for {type(step)}")
+
+                # table = wrapper.to_ibis()(table)
+
+            return table
+
+        return fn
+
+# t = ibis.memtable(dataInfo, name="The_Impact_of_Space_on_Mice")
+t = ibis.memtable(dataInfo)
+# t = ibis.array(t)
+t = t.drop("gene")
+
+pipeline = PipelineIbis(pipe)
+print("HI HOW ARE YOU?")
+result = pipeline.to_ibis()(t)
 
 # Get the kbest object
 # Get the boolean mask indicating which features were selected
@@ -116,7 +204,6 @@ selected_mask = kbest.get_support()
 all_feature_names = dataInfo.columns.tolist()
 selected_feature_names = [all_feature_names[i] for i in range(min(len(selected_mask), len(all_feature_names)))
                           if selected_mask[i]]
-print("Selected feature names:", selected_feature_names)
 
 pca = pipe.named_steps["pca"]
 variances = pca.explained_variance_ratio_
@@ -125,17 +212,22 @@ logistic = pipe.named_steps['logistic']
 coefficients = logistic.coef_
 intercept = logistic.intercept_
 
-# forest = pipe.named_steps['rf']
-# print(forest.feature_importances_)
-
-# Use cross_val_score to perform cross-validation
-scores = cross_val_score(pipe, X, y, cv=5) # 5-fold cross-validation
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
 y_pred = pipe.predict(X)
 accuracy = accuracy_score(y, y_pred)
 
+print("Selected feature names:", selected_feature_names)
 print("Variances", variances)
 print('Coefficients:', coefficients)
 print('Intercept:', intercept)
 print('Accuracy score:', accuracy)
+
+# # Reference Link: https://github.com/ibis-project/ibis-examples/blob/main/examples/Substrait.ipynb
+
+# Do this instead of dubkdb:
+# https://github.com/tokoko/substrait-ml/blob/main/sklearn_ibis/pipeline/pipeline_ibis.py
+
+# Another Reference
+# https://ibis-project.org/ibis-for-sql-programmers/#non-trivial-grouping-keys
+
+# Command
+#sys.path.append(f'{os.getcwd()}/substrait-ml')
